c7b4a419b0a2a18f5ae1cee9e5a63786c1a03966
Pedro Lucas <pedroldl123@gmail.com>
2023-04-19 11:08:48 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import './App.css';
+import React, { useEffect, useState } from 'react'
+import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
+import Header from './components/Header';
+
+function App() {
+
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+
+      let originals = list.filter(i => i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo);
+    }
+
+    loadAll();
+  }, []);
+
+  useEffect(() => {
+    const scrollListener = () => {
+      if(window.scrollY > 10) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    }
+
+    window.addEventListener('scroll', scrollListener);
+
+    return () => {
+      window.removeEventListener('scroll', scrollListener);
+    }
+  }, [])
+
+  return (
+    <div className="page">
+
+      <Header black={blackHeader} />
+
+      {featuredData &&
+        <FeaturedMovie item={featuredData} />
+      }
+
+      <section className='lists'>
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+
+      <footer>
+        Feito por Pedroolcs<br/>
+        Direitos de imagem para Netflix<br/>
+        Dados pegos do site <a href='https://www.themoviedb.org/'>Themoviedb.org</a>
+      </footer>
+
+      {movieList.length <= 0 &&
+      <div className='loading'>
+          <img src='https://netflix-clone-react-jsc.netlify.app/static/media/loading.948201348313a3185f33.gif' alt='Carregando' />
+      </div>
+      }
+    </div>
+  );
+}
+
+export default App;
